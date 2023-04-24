4169dc35cf70127c83eb424860a9c2bbe70e3d67
J Cody Eisenbach <ceisenbach@gmail.com>
2023-04-24 02:17:04 -0500

    card snap back

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-
+    private GameObject Canvas;
     private bool isDragging = false;
     private bool isOverDropZone = false;
     private GameObject DropZone;
+    private GameObject startParent;
     private Vector2 startPosition;
+
+    private void Start()
+    {
+        Canvas = GameObject.Find("MainCanvas");
+    }
 
     void Update()
     {
         if (isDragging)
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
         }
     }
 
@@ -33,7 +40,7 @@ public class DragDrop : MonoBehaviour
 
     public void StartDrag()
     {
-
+        startParent = transform.parent.gameObject;
         startPosition = transform.position;
         isDragging = true;
     }
@@ -48,6 +55,7 @@ public class DragDrop : MonoBehaviour
         else
         {
             transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
         }
     }
 }
